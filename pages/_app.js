8d587897b554804c5e4400982ed2360d475a244c
Satria Pratama Taruna <satriapratamat7@gmail.com>
2021-12-23 17:52:28 +0700

    add admin routing

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ import theme from '../styles/theme';
 import { createEmotionCache } from '../src/utils/createEmotionCache';
 import DateAdapter from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
-// import { RootLayout } from '../src/components/Layout/RootLayout';
+import { RootLayout } from '../src/components/Layout/RootLayout';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -17,16 +17,16 @@ export default function MyApp({ Component, emotionCache = clientSideEmotionCache
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>Spill</title>
+        <title>CalFit Admin</title>
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <LocalizationProvider dateAdapter={DateAdapter}>
-          {/* <RootLayout> */}
-          <Component {...pageProps} />
-          {/* </RootLayout> */}
+          <RootLayout>
+            <Component {...pageProps} />
+          </RootLayout>
         </LocalizationProvider>
       </ThemeProvider>
     </CacheProvider>
---
+++ src/components/Layout/ContainerLayoutStyles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@mui/styles"
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        display: "flex",
+        margin: "auto",
+        padding: "20px",
+    },
+}));
