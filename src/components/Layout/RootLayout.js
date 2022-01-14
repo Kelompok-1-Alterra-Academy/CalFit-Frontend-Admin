3992f224d89b8df5ffe01a2e7b6da5593bcae3fe
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-01-14 18:09:23 +0700

    feat: slicing login page design

--- src/components/Layout/RootLayout.js
+++ src/components/Layout/RootLayout.js
@@ -1,20 +1,21 @@
 import React from "react";
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 import PropTypes from "prop-types";
-import {ContainerLayout} from "./ContainerLayout";
+import { ContainerLayout } from "./ContainerLayout";
 
 export const RootLayout = ({ children }) => {
 
-    return (
-      <Box>
-        <ContainerLayout>
-          <Box>{children}</Box>
-        </ContainerLayout>
-      </Box>
-    );
-  };
-  
-  RootLayout.propTypes = {
-    children: PropTypes.oneOfType([PropTypes.array, PropTypes.element]),
-  };
-  
+  return (
+    <Box style={{
+      backgroundColor: "#f6f6f6"
+    }}>
+      <ContainerLayout>
+        <Box>{children}</Box>
+      </ContainerLayout>
+    </Box>
+  );
+};
+
+RootLayout.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.array, PropTypes.element]),
+};
