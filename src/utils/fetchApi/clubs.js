8b74d1e562d21dfd17200f041b003115b9fb7eac
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-01-15 14:47:31 +0700

    feat: display clubs data on table

---
+++ src/utils/fetchApi/clubs.js
@@ -0,0 +1,25 @@
+import baseApi from './api';
+
+export const getAllGyms = async (setLoadingState, setData, { limit, page }) => {
+  setLoadingState(true);
+  return baseApi
+    .get(`/gyms?limit=${limit}&page=${page}`)
+    .then((res) => {
+      setData(res.data.data ?? []);
+    })
+    .finally(() => {
+      setLoadingState(false);
+    });
+};
+
+export const getGymById = async (setLoadingState, setData, id) => {
+  setLoadingState(true);
+  return baseApi
+    .get(`/gyms/${id}`)
+    .then((res) => {
+      setData(res.data.data ?? []);
+    })
+    .finally(() => {
+      setLoadingState(false);
+    });
+};
