1935e6bdbefc9949a0c44bdb7d37d69d31805015
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-01-15 14:47:00 +0700

    feat: add clubs table

--- pages/superadmin/clubs/index.js
+++ pages/superadmin/clubs/index.js
@@ -1,11 +1,20 @@
+import { useState, useEffect } from 'react';
 import Head from "next/head";
 import MaterialTable from "material-table";
 import { useStyles } from "../../../styles/clubs/Index.style";
 import { TopBar } from "../../../src/components/navigation/TopBar";
 import { MenuBar } from "../../../src/components/navigation/MenuBar";
+import { tableIcons } from "../../../src/components/table/MaterialTable";
+import { getAllGyms } from "../../../src/utils/fetchApi/clubs";
 
 export default function DashboardSuperAdmin() {
   const classes = useStyles();
+  const [clubs, setClubs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getAllGyms(setLoading, setClubs, { limit: 1000, page: 1 });
+  }, []);
 
   return (
     <div className={classes.root}>
@@ -20,35 +29,27 @@ export default function DashboardSuperAdmin() {
       <main className={classes.main}>
         <MenuBar selected={"Clubs"} />
         <MaterialTable
-          title="Positioning Actions Column Preview"
+          title="Clubs"
+          icons={tableIcons}
           columns={[
             { title: 'Name', field: 'name' },
-            { title: 'Surname', field: 'surname' },
-            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-            { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
             {
-              title: 'Birth Place',
-              field: 'birthCity',
-              lookup: { 34: 'İstanbul', 63: 'Şanliurfa' },
+              title: 'Admin', field: 'operationalAdminId',
             },
+            { title: 'Telephone', field: 'telephone' },
           ]}
-          data={[
-            { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-            { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-          ]}
+          data={clubs}
           actions={[
             {
-              icon: 'save',
-              tooltip: 'Save User',
+              icon: tableIcons.Edit,
+              tooltip: 'Edit Club',
               onClick: (event, rowData) => alert("You saved " + rowData.name)
             },
             rowData => ({
-              icon: 'delete',
-              tooltip: 'Delete User',
+              icon: tableIcons.Delete,
+              tooltip: 'Delete Club',
               onClick: (event, rowData) => confirm("You want to delete " + rowData.name),
-              disabled: rowData.birthYear < 2000
+              // disabled: rowData.birthYear < 2000
             })
           ]}
           options={{
