3a0ec6ce7872f0cef930efa62295f54fbb0bf4c6
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2022-01-24 20:45:16 +0700

    feat: update password

---
+++ src/utils/jwtDecode/jwtDecode.js
@@ -0,0 +1,16 @@
+import { parseCookies } from "nookies";
+
+export default function jwtDecode() {
+  const { token } = parseCookies();
+  if (!token) return {
+    Id: 0,
+    Email: "",
+    Member: false,
+    OperationalAdmin: false,
+    Superadmin: true,
+    exp: 0,
+  };
+  const decode = token.split(".")[1];
+  const base64 = decode.replace("-", "+").replace("_", "/");
+  return JSON.parse(window.atob(base64));
+}
