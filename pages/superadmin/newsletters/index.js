2770f1e0d640a80f7080c9004bbd6c0c02eae9f4
Satria Pratama Taruna <satriapratamat7@gmail.com>
2022-01-28 08:54:19 +0700

    add newsletters page at superadmin

--- pages/superadmin/newsletters/index.js
+++ pages/superadmin/newsletters/index.js
@@ -7,10 +7,33 @@ import { useStyles } from '../../../styles/classes/Index.style';
 import { TopBar } from '../../../src/components/navigation/TopBar';
 import { MenuBar } from '../../../src/components/navigation/MenuBar';
 import { tableIcons } from '../../../src/components/table/MaterialTable';
+import { getAllNewsletters, deleteNewsletter } from '../../../src/utils/fetchApi/newsletters';
 
 export default function NewslettersSuperAdmin() {
   const styles = useStyles();
   const router = useRouter();
+  const [newslettersList, setNewslettersList] = useState([]);
+  const [alertNewsletters, setAlertNewsletters] = useState({
+    status: false,
+    message: '',
+  })
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getAllNewsletters(setLoading, setNewslettersList, { limit: 1000, page: 1 });
+  }, []);
+
+  const handleDelete = async (id) => {
+    const res = await deleteClass(setLoading, setAlertNewsletters, Id);
+    console.log(res);
+    if (res.status === 200) {
+      setNewslettersList(newslettersList.filter((newsItem) => newsItem.id !== id));
+      return true;
+    }
+    return false;
+  };
+
+
   return (
     <div className={styles.root}>
       <Head>
@@ -27,6 +50,50 @@ export default function NewslettersSuperAdmin() {
             <MenuBar selected={'Newsletters'} />
           </Grid>
           <Grid item xs={9}>
+            <MaterialTable
+                className={styles.table}
+                title='Newsletters'
+                icons={tableIcons}
+                columns={[
+                  { title: 'Id', field: 'id' },
+                  { title: 'Title', field: 'title',  width: '20%'},
+                  { title: 'Description', field: 'description'},
+                  { title: 'Url Picture', field: 'url_picture', width: '10%' },
+                  { title: 'Content', field: 'content' },
+                  { title: 'Operational Admin Id', field: 'operationalAdminId', width: '10%'},
+                ]}
+                data={classesList}
+                actions={[
+                  {
+                    icon: tableIcons.Edit,
+                    tooltip: 'Edit Newsletters',
+
+                    onClick: (event, rowData) =>
+                      router.push(`/superadmin/newsletters/${rowData.id}/edit`),
+                  },
+                  (rowData) => ({
+                    icon: tableIcons.Delete,
+                    tooltip: 'Delete Newsletters',
+                    onClick: (event, rowData) => {
+                      const isDelete = confirm(`You want to delete ${rowData.title}(id: ${rowData.id}) ?`);
+                      if (isDelete) {
+                        const success = handleDelete(rowData.id);
+                        if (success) alert(`You deleted ${rowData.title}(id: ${rowData.id})`);
+                        else alert(`Can't delete ${rowData.title}(id: ${rowData.id})`);
+                      }
+                    },
+                  }),
+                  // {
+                  //   icon: tableIcons.Add,
+                  //   tooltip: 'Add New Class',
+                  //   isFreeAction: true,
+                  //   onClick: (event) => router.push('/superadmin/classes/add'),
+                  // },
+                ]}
+                options={{
+                  actionsColumnIndex: -1,
+                }}
+              />
           </Grid>
         </Grid>
       </main>
